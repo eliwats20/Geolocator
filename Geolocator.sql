34a3580f05dbcf054c714d67ed4ba9b1b0155155
eliwats20 <150206903+eliwats20@users.noreply.github.com>
2024-10-06 23:19:39 -0400

    MYSQL DATABASE CONNECTION

--- Geolocator.sql
+++ Geolocator.sql
@@ -1,6 +1,6 @@
-drop table log_query if exists;
-drop table table_id if exists;
-drop table ip_id if exists;
+drop table if exists log_query;
+drop table if exists table_id;
+drop table if exists ip_id;
 
 
 
@@ -26,13 +26,13 @@ create table log_query (
 	region varchar(30),
 	country varchar(30),
 	postal_code NUMERIC(5,0),
-	latitute varchar (10),
+	latitude varchar (10),
 	longitude varchar (10),
 	query_time varchar(40),
 	request_type varchar(4),
 	request_resource varchar(40),
 	HTTP_response_code NUMERIC(3,0),
-	object_size NUMERIC(5,0)
+	object_size NUMERIC(5,0),
 	foreign key (log_id) references table_id(log_id)
 );
 
